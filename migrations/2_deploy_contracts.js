bb9bfcd085d56522502f65ed7223f7778c011f34
ceruleanhu <hulandan123@gmail.com>
2020-09-02 22:11:20 +0800

    Issue SWAP-12: only explicitly deploy new erc20 token and staking contracts for development and ganache env for now

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,13 +3,16 @@ const ERC20 = artifacts.require('@openzeppelin-solidity/contracts/token/ERC20/St
 const ERC900BasicStakeContract = artifacts.require('BasicStakeContractMock')
 module.exports = function (deployer, network, accounts) {
   // only for local network, deploy a plain ERC20
+  if (['development', 'ganache'].indexOf(network) > -1) {
 
-  deployer.deploy(ERC20, { gas: 6721970 }).then(async () => {
-    console.log(`Deployed ERC20 token address : ${ERC20.address}`)
-    const ONE_YEAR_IN_SECONDS = 31557600
-    await deployer.deploy(ERC900BasicStakeContract, ERC20.address, ONE_YEAR_IN_SECONDS, { gas: 6721970 })
-    await deployer.deploy(ERC900BasicStakeContract, ERC20.address, ONE_YEAR_IN_SECONDS * 2, { gas: 6721970 })
-    await deployer.deploy(ERC900BasicStakeContract, ERC20.address, ONE_YEAR_IN_SECONDS * 3, { gas: 6721970 })
-    deployer.deploy(ERC900BasicStakeContract, ERC20.address, ONE_YEAR_IN_SECONDS * 4, { gas: 6721970 })
-  })
+    deployer.deploy(ERC20, { gas: 6721970 }).then(async () => {
+      console.log(`Deployed ERC20 token address : ${ERC20.address}`)
+      const ONE_YEAR_IN_SECONDS = 31557600
+      await deployer.deploy(ERC900BasicStakeContract, ERC20.address, ONE_YEAR_IN_SECONDS, { gas: 6721970 })
+      await deployer.deploy(ERC900BasicStakeContract, ERC20.address, ONE_YEAR_IN_SECONDS * 2, { gas: 6721970 })
+      await deployer.deploy(ERC900BasicStakeContract, ERC20.address, ONE_YEAR_IN_SECONDS * 3, { gas: 6721970 })
+      deployer.deploy(ERC900BasicStakeContract, ERC20.address, ONE_YEAR_IN_SECONDS * 4, { gas: 6721970 })
+    })
+
+  }
 }
