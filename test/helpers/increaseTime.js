a1f0a72750398183547221eb5ea88cc634d6ce2b
ceruleanhu <hulandan123@gmail.com>
2020-09-03 20:35:41 +0800

    Issue SWAP-12: removed not gonna use contracts and fixed all existing test by dependency version upgrade and sysntax fix

--- test/helpers/increaseTime.js
+++ test/helpers/increaseTime.js
@@ -1,6 +1,6 @@
 export default (seconds) => {
   return new Promise((resolve, reject) => {
-    web3.currentProvider.sendAsync({
+    web3.currentProvider.send({
       jsonrpc: '2.0',
       method: 'evm_increaseTime',
       params: [seconds],
